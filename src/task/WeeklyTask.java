4848546be8461c78bb811617a273d802e3342e5b
eliza <eliza.an.popova@gmail.com>
2023-02-24 23:51:28 +0300

    final

---
+++ src/task/WeeklyTask.java
@@ -0,0 +1,18 @@
+package task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class WeeklyTask extends Task {
+    public WeeklyTask(String title, Type type, int id, LocalDateTime dateTime, String description) {
+        super(title, type, id, dateTime, description);
+    }
+
+    public boolean appearsIn(LocalDate localDate) {
+        return localDate.isAfter(getDateTime().toLocalDate())
+                || localDate.isEqual(getDateTime().toLocalDate())
+                && localDate.getDayOfWeek() == getDateTime().getDayOfWeek();
+    }
+
+
+}
---
+++ src/task/YearlyTask.java
@@ -0,0 +1,18 @@
+package task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class YearlyTask extends Task {
+
+    public YearlyTask(String title, Type type, int id, LocalDateTime dateTime, String description) {
+        super(title, type, id, dateTime, description);
+    }
+
+    public boolean appearsIn(LocalDate localDate) {
+        return localDate.isAfter(getDateTime().toLocalDate())
+                || localDate.isEqual(getDateTime().toLocalDate())
+                && localDate.getDayOfYear() == getDateTime().getDayOfYear();
+    }
+
+}
